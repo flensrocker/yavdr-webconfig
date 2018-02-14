ac8b4a90059fc038fa73f5d4e6ee8140d767d976
Lars Hanisch <dvb@flensrocker.de>
2018-02-14 12:00:19 +0100

    use service-worker only, if enabled

--- src/app/tools/update.service.ts
+++ src/app/tools/update.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
 import { UpdateAvailableEvent } from '@angular/service-worker/src/low_level';
 import { Observable } from 'rxjs/Observable';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { interval } from 'rxjs/observable/interval';
 import { UpdateData } from './update.servicedata';
 
@@ -10,26 +10,32 @@ export { UpdateData };
 
 @Injectable()
 export class UpdateService {
-  private _available: ReplaySubject<UpdateData> = new ReplaySubject<UpdateData>(1);
+  private _available: BehaviorSubject<UpdateData> = new BehaviorSubject<UpdateData>(new UpdateData('', ''));
   public get available(): Observable<UpdateData> {
     return this._available.asObservable();
   }
 
   constructor(private _swUpdate: SwUpdate) {
-    _swUpdate.available.subscribe((e: UpdateAvailableEvent) => {
-      this._available.next(new UpdateData(e.current.hash, e.available.hash));
-    });
-    interval(60 * 1000).subscribe(() => this.checkForUpdate());
+    if (this._swUpdate.isEnabled) {
+      this._swUpdate.available.subscribe((e: UpdateAvailableEvent) => {
+        this._available.next(new UpdateData(e.current.hash, e.available.hash));
+      });
+      interval(60 * 1000).subscribe(() => this.checkForUpdate());
+    }
   }
 
   public checkForUpdate(): void {
-    this._swUpdate.checkForUpdate();
+    if (this._swUpdate.isEnabled) {
+      this._swUpdate.checkForUpdate();
+    }
   }
 
   public updateApp(): void {
-    this._swUpdate
-      .activateUpdate()
-      .then(() => document.location.href = '/');
-    // .then(() => document.location.reload()); // needs HTML5 pushstate http-server
+    if (this._swUpdate.isEnabled) {
+      this._swUpdate
+        .activateUpdate()
+        .then(() => document.location.href = '/');
+      // .then(() => document.location.reload()); // needs HTML5 pushstate http-server
+    }
   }
 }
