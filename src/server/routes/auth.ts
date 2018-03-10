afeef8f5eddc5f89e26678e57cd5261b136d3d3c
Lars Hanisch <dvb@flensrocker.de>
2018-03-10 12:14:20 +0100

    server - don't authenticate logout route

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -6,5 +6,5 @@ export const AuthRoutes = Routing.setupRoutes([
     new Route<ValidateResponse>('get', '/login', new AsyncRouteHandler(Auth.validate), false),
     new Route<LoginResponse>('post', '/login', new AsyncRouteHandler(Auth.login), false),
     new Route<LoginTokenResponse>('post', '/login/token', new AsyncRouteHandler(Auth.token), false),
-    new Route<LogoutResponse>('post', '/logout', new SyncRouteHandler(Auth.logout), true),
+    new Route<LogoutResponse>('post', '/logout', new SyncRouteHandler(Auth.logout), false),
 ]);
